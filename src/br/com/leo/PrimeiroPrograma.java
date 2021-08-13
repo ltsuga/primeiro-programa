df7fb4762f8095531432cc554f17f0d45033d9eb
ltsuga <85001056+ltsuga@users.noreply.github.com>
2021-08-12 23:06:01 -0300

    Initial commit

---
+++ src/br/com/leo/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.leo;
+
+import br.com.leo.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
