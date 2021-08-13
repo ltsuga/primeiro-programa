f74c24b6ab41419bcd3aa21270a1283f8da83704
ltsuga <85001056+ltsuga@users.noreply.github.com>
2021-08-12 23:13:29 -0300

    Added Livro class

--- src/br/com/leo/PrimeiroPrograma.java
+++ src/br/com/leo/PrimeiroPrograma.java
@@ -5,6 +5,47 @@ import br.com.leo.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+
+        Livro livro = new Livro("Naruto",150);
+
+        System.out.println(livro);
         System.out.println(gato);
+    }
+
+
+
+}
+
+class Livro{
+    private String nome;
+    private Integer paginas;
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
     }
 }
